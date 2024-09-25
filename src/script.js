ca061022dcbed6775ac2b4212b8218c265d71fdb
Yehor Honcharov <honcharovstallker@gmail.com>
2024-09-25 16:07:03 +0300

    home work

--- src/script.js
+++ src/script.js
@@ -7,6 +7,10 @@ const app = express()
 const PORT = 8000
 const HOST = 'localhost'
 
+app.set('view engine', 'ejs')
+//встановлюємо папки з шаблонами для ejs
+app.set('views', path.join(__dirname, 'templates'))
+
 app.use('/static/', express.static(path.join(__dirname, 'static')))
 
 function getDate(){
@@ -14,14 +18,38 @@ function getDate(){
 }
 
 app.get('/', (req, res) => {
-    // console.log("ktoto zashel na stranicu")
     res.sendFile(path.resolve(__dirname, "./templates/index.html"))
+    const context = {
+        // words: ['hello', 'world', 'rinat']
+        title: 'posts of the day'
+     }
+    res.render('index', context)
 })
 
-app.get('/date', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./templates/date.html"))
+app.get('/date/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/templates/date.html"))
     getDate()
 })
+
+app.get('/posts/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./templates/posts.html"))
+    const context = {
+        posts: [{name1: 'Как готовить ВКУСНУЮ, СОЧНУЮ аля-шаурму', author1: 'Толік from Tajikistan'}, {name2: 'Новости дня', author2: 'Кіріл Орешкін'}]
+    }
+    res.render('posts', context)
+})
+
+// function getCurrentDate() {
+//     const now = new Date(); 
+//     return now.toString(); 
+// }
+
+// app.get('/date/', (req, res) => {
+//     const currentDate = getCurrentDate(); 
+//     console.log('Текущая дата и время:', currentDate);
+//     res.send(`Текущая дата и время: ${currentDate}`);
+    
+// });
 
 app.listen(PORT, HOST, () =>{
     console.log("http://localhost:8000")
