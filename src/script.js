5dd0aaa3691428e24b8e818e7bb80071d3821db6
Yehor Honcharov <honcharovstallker@gmail.com>
2024-10-02 19:03:31 +0300

    add post:id 1 2 3 4

--- src/script.js
+++ src/script.js
@@ -2,10 +2,40 @@ const moment = require("moment")
 const express = require('express')
 const path = require("path")
 
+const posts = [
+    {
+        "name": 'Фото дня',
+        "src": "https://www.spcdn.org/blog/wp-content/uploads/2021/05/Photo-of-the-day.png",
+        "date": "2022-10-12 ",
+        "description": "красивые фоточки",
+        "author": "Телеканал Дождь"
+    },
+    {
+        "name": 'Цитати',
+        "src": "https://www.spcdn.org/blog/wp-content/uploads/2021/05/Quote.png",
+        "date" : "2024-01-01",
+        "description": "«вдохновляющие» цитаты",
+        "author": "Словарный запас"
+    },
+    {
+        "name": 'Опрос',
+        "src": "https://www.spcdn.org/blog/wp-content/uploads/2021/05/Poll.png",
+        "date" : "2023-03-16 0:10",
+        "description": "У тебя есть полковая техника?",
+        "author": "War Thunder"
+
+    },
+    {
+        "name": 'Современная архитектура полярный станций',
+        "src": "https://www.spcdn.org/blog/wp-content/uploads/2021/05/Compilation.png",
+        "date" : "2024-01-10 14:20",
+        "description": "Современная архитектура полярный станций",
+        "author": "Academic Architecture"
+    }
+]
+
 const app = express()
 
-const PORT = 8000
-const HOST = 'localhost'
 
 app.set('view engine', 'ejs')
 //встановлюємо папки з шаблонами для ejs
@@ -37,7 +67,25 @@ app.get('/posts/', (req, res) => {
     const context = {
         posts: [{name1: 'Как готовить ВКУСНУЮ, СОЧНУЮ аля-шаурму', author1: 'Толік from Tajikistan'}, {name2: 'Новости дня', author2: 'Кіріл Орешкін'}]
     }
+    console.log(req.query)
+    const max = req.query.max
+    if (max <= posts.length) {
+        context.posts = posts.slice(0, max)
+    }
     res.render('posts', context)
+})
+
+app.get('/post/:id', (req, res) => {
+    console.log(req.params.id)
+    const id = req.params.id
+    const context = {
+        post: posts[id-1]
+    }
+    if (id <= posts.length){
+        res.render('post', context)
+    } else{
+        res.send("таких постов нет")
+    }
 })
 
 // function getCurrentDate() {
@@ -56,6 +104,9 @@ app.get('/user/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./templates/user.html"));
     res.render('user')
 })
+
+const PORT = 8000
+const HOST = 'localhost'
 
 app.listen(PORT, HOST, () =>{
     console.log("http://localhost:8000")
